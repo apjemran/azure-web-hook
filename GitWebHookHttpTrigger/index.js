a27e019101c38a0a69f84c3445a448bec4e68a40
Khan, Mohd Emran (mk3993) <mk3993@att.com>
2021-04-07 13:56:36 +0530

    Web hook function changes

--- GitWebHookHttpTrigger/index.js
+++ GitWebHookHttpTrigger/index.js
@@ -1,12 +1,16 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.body.pages[0].title){
+    if (req.body.pages && req.body.pages[0] && req.body.pages[0].title){
         context.res = {
             body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
         };
-    }
-    else {
+    }else if(req.body.pusher){
+        context.res = {
+            status: 200,
+            body: "Code Push By "+ req.body.pusher.name
+        }        
+    }else {
         context.res = {
             status: 400,
             body: ("Invalid payload for Wiki event")
